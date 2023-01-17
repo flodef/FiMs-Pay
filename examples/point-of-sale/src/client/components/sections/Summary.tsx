ababb32b2e9844be7fc3eb15fa55a2314b3a610f
Flo <flodef@free.fr>
2023-01-17 19:57:43 +0100

    POS: Current balance is now displayed

--- examples/point-of-sale/src/client/components/sections/Summary.tsx
+++ examples/point-of-sale/src/client/components/sections/Summary.tsx
@@ -1,29 +1,39 @@
+import { useWallet } from "@solana/wallet-adapter-react";
 import React, { FC } from 'react';
 import { FormattedMessage } from "react-intl";
-import { useConfig } from '../../hooks/useConfig';
 import { usePayment } from '../../hooks/usePayment';
 import { Amount } from './Amount';
 import css from './Summary.module.css';
 
 export const Summary: FC = () => {
     const { amount, balance } = usePayment();
+    const { publicKey } = useWallet();
 
     return (
         <div className={css.root}>
-            <div className={css.title}><FormattedMessage id="balance" /></div>
-            {balance !== undefined
-                ? balance > 0
-                    ? <Amount value={balance} />
-                    : <FormattedMessage id="emptyBalance" />
-                : <FormattedMessage id="balanceLoading" />}
+            <div className={css.title}>
+                <FormattedMessage id="balance" />
+            </div>
+            <div className={css.balance}>
+                {publicKey
+                    ? balance !== undefined
+                        ? balance > 0
+                            ? <Amount value={balance} />
+                            : <FormattedMessage id="emptyBalance" />
+                        : <FormattedMessage id="balanceLoading" />
+                    : <FormattedMessage id="walletNotConnected" />
+                }
+            </div>
             <div className={css.total}>
-                <div className={css.totalLeft}><FormattedMessage id="total" /></div>
+                <div className={css.totalLeft}>
+                    <FormattedMessage id="total" />
+                </div>
                 <div className={css.totalRight}>
                     <div className={css.amount}>
                         <Amount value={amount} />
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
