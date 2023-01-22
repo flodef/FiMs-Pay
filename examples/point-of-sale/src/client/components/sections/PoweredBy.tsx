67859006b42e13affa805ea93a1a60cca6a53d0a
Flo <flodef@free.fr>
2023-01-22 20:48:42 +0100

    PAY: Add custom logo

--- examples/point-of-sale/src/client/components/sections/PoweredBy.tsx
+++ examples/point-of-sale/src/client/components/sections/PoweredBy.tsx
@@ -1,13 +1,13 @@
 import React, { FC } from 'react';
 import { FormattedMessage } from "react-intl";
-import { SolanaPayLogo } from '../images/SolanaPayLogo';
+import { FiMsPayLogo } from '../images/FiMsPayLogo';
 import css from './PoweredBy.module.css';
 
 export const PoweredBy: FC = () => {
     return (
         <div className={css.root}>
             <FormattedMessage id="poweredBy" />
-            <SolanaPayLogo />
+            <FiMsPayLogo />
         </div>
     );
 };
