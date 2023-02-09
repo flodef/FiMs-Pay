419ad65e6e2148497878304cc5087044e8595184
Flo <flodef@free.fr>
2023-02-09 17:05:50 +0100

    Alert dialog <p> bug fix

--- src/client/components/sections/AlertDialogPopup.tsx
+++ src/client/components/sections/AlertDialogPopup.tsx
@@ -23,42 +23,45 @@ export interface AlertDialogPopupProps {
 }
 
 export const AlertDialogPopup: FC<AlertDialogPopupProps> = ({ button, onClick, alert }) => {
-    const buttonColor = alert ? alert.type === AlertType.Message ? css.green : alert.type === AlertType.Warning ? css.orange : css.red : undefined;
+    const buttonColor = alert
+        ? alert.type === AlertType.Message
+            ? css.green
+            : alert.type === AlertType.Warning
+            ? css.orange
+            : css.red
+        : undefined;
 
-    return !alert
-        ? (
-            <div>
-                {button}
-            </div>
-        )
-        : (
-            <AlertDialog.Root>
-                <AlertDialog.Trigger asChild>
-                    {button}
-                </AlertDialog.Trigger>
-                <AlertDialog.Portal>
-                    <AlertDialog.Overlay className={css.AlertDialogOverlay} />
-                    <AlertDialog.Content className={css.AlertDialogContent}>
-                        <AlertDialog.Title className={css.AlertDialogTitle}>
-                            {alert.title}
-                        </AlertDialog.Title>
-                        <AlertDialog.Description className={css.AlertDialogDescription}>
-                            {alert.description.map(x => <p key={x}>{x}<br /></p>)}
-                        </AlertDialog.Description>
-                        <div className={css.AlertDialogButton}>
-                            <AlertDialog.Cancel asChild>
-                                <button className={classNames(css.Button, css.mauve)}>
-                                    <FormattedMessage id="cancel" />
-                                </button>
-                            </AlertDialog.Cancel>
-                            <AlertDialog.Action asChild>
-                                <button className={classNames(css.Button, buttonColor)} onClick={onClick}>
-                                    <FormattedMessage id="OK" />
-                                </button>
-                            </AlertDialog.Action>
-                        </div>
-                    </AlertDialog.Content>
-                </AlertDialog.Portal>
-            </AlertDialog.Root>
-        );
+    return !alert ? (
+        <div>{button}</div>
+    ) : (
+        <AlertDialog.Root>
+            <AlertDialog.Trigger asChild>{button}</AlertDialog.Trigger>
+            <AlertDialog.Portal>
+                <AlertDialog.Overlay className={css.AlertDialogOverlay} />
+                <AlertDialog.Content className={css.AlertDialogContent}>
+                    <AlertDialog.Title className={css.AlertDialogTitle}>{alert.title}</AlertDialog.Title>
+                    <AlertDialog.Description className={css.AlertDialogDescription}>
+                        {alert.description.map((x) => (
+                            <span key={x}>
+                                {x}
+                                <br />
+                            </span>
+                        ))}
+                    </AlertDialog.Description>
+                    <div className={css.AlertDialogButton}>
+                        <AlertDialog.Cancel asChild>
+                            <button className={classNames(css.Button, css.mauve)}>
+                                <FormattedMessage id="cancel" />
+                            </button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action asChild>
+                            <button className={classNames(css.Button, buttonColor)} onClick={onClick}>
+                                <FormattedMessage id="OK" />
+                            </button>
+                        </AlertDialog.Action>
+                    </div>
+                </AlertDialog.Content>
+            </AlertDialog.Portal>
+        </AlertDialog.Root>
+    );
 };
