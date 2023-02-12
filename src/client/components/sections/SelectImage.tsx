8675707ed96c6b101a2a45d99e59b3a5d456c98d
Flo <flodef@free.fr>
2023-02-12 02:02:02 +0100

    Display currency's icon with custom size

--- src/client/components/sections/SelectImage.tsx
+++ src/client/components/sections/SelectImage.tsx
@@ -3,24 +3,41 @@ import * as Select from '@radix-ui/react-select';
 import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
 import css from './SelectImage.module.css';
 
+const getDefaultData = (item: string) => {
+    return { key: item, value: item, text: item };
+};
+
+interface SelectItemProps {
+    item: any;
+    size: string | number | undefined;
+    getData: (item: any) => { key: string | number; value: string; text: string };
+    getImage?: (value: string) => JSX.Element;
+    imageOnly?: boolean;
+}
+
+const SelectItem: FC<SelectItemProps> = ({ item, size, getData, getImage, imageOnly }) => (
+    <div className={css.SelectedItem}>
+        <div style={{ width: size, height: size }}>{getImage ? getImage(getData(item).value) : null}</div>
+        {!imageOnly ? getData(item).text : null}
+    </div>
+);
+
 export interface SelectImageProps {
     id: string;
     options: Array<any>;
     value: string;
+    size?: string | number | undefined;
     onValueChange: (value: string) => void;
     getData?: (item: any) => { key: string | number; value: string; text: string };
     getImage?: (value: string) => JSX.Element;
     imageOnly?: boolean;
 }
 
-const getDefaultData = (item: string) => {
-    return { key: item, value: item, text: item };
-};
-
 export const SelectImage: FC<SelectImageProps> = ({
     id,
     options,
     value,
+    size = 32,
     onValueChange,
     getData,
     getImage,
@@ -33,19 +50,16 @@ export const SelectImage: FC<SelectImageProps> = ({
     value ??= defaultValue;
 
     return options.length === 1 ? (
-        <div className={css.SelectedItem}>
-            {getImage ? getImage(options[0]) : null}
-            {!imageOnly ? getData(options[0]).text : null}
-        </div>
+        <SelectItem item={options[0]} size={size} getData={getData} getImage={getImage} imageOnly={imageOnly} />
     ) : (
         <Select.Root onValueChange={onValueChange} value={value} defaultValue={defaultValue}>
-            <Select.Trigger className={css.SelectTrigger} aria-label={id}>
+            <Select.Trigger className={css.SelectTrigger} style={{ height: Number(size) + 3 }} aria-label={id}>
                 <Select.Value />
                 <Select.Icon className={css.SelectIcon}>
                     <ChevronDownIcon />
                 </Select.Icon>
             </Select.Trigger>
-            <Select.Portal id={id} style={{ zIndex: 1 }}>
+            <Select.Portal id={id}>
                 <Select.Content className={css.SelectContent}>
                     <Select.ScrollUpButton className={css.SelectScrollButton}>
                         <ChevronUpIcon />
@@ -54,12 +68,20 @@ export const SelectImage: FC<SelectImageProps> = ({
                         {options.map((item) => {
                             const { key, value, text } = getData ? getData(item) : getDefaultData(item);
                             return (
-                                <Select.Item className={css.SelectItem} key={key} value={value}>
+                                <Select.Item
+                                    className={css.SelectItem}
+                                    style={{ height: Number(size) + 13 }}
+                                    key={key}
+                                    value={value}
+                                >
                                     <Select.ItemText>
-                                        <div className={css.SelectedItem}>
-                                            {getImage ? getImage(value) : null}
-                                            {!imageOnly ? text : null}
-                                        </div>
+                                        <SelectItem
+                                            item={item}
+                                            size={size}
+                                            getData={getData ?? getDefaultData}
+                                            getImage={getImage}
+                                            imageOnly={imageOnly}
+                                        />
                                     </Select.ItemText>
                                     <Select.ItemIndicator className={css.SelectItemIndicator}>
                                         <CheckIcon />
