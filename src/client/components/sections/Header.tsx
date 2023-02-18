7732cb20738e154c9c984c13e52140482e0bf4f9
Flo <flodef@free.fr>
2023-02-18 20:31:39 +0100

    Add google console verification

--- src/client/components/sections/Header.tsx
+++ src/client/components/sections/Header.tsx
@@ -10,6 +10,7 @@ export const Header: FC<HeaderProps> = ({ label }) => {
     return (
         <Head>
             <title>{(label && label !== APP_TITLE ? label + ' @ ' : '') + APP_TITLE}</title>
+            <meta name="google-site-verification" content="MsiwZHuFSJ2SGGT-MzDAiPwWZmK2F0PiLWwOsHb78Ck" />
         </Head>
     );
 };
