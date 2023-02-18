f83044574f539663bb8e8e41dae8ec0b0bf72ea2
Flo <flodef@free.fr>
2023-02-18 21:10:29 +0100

    Add google console verification 2/n

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html style={{ visibility: 'hidden' }}>
-            <Head />
+            <Head>
+                <meta name="google-site-verification" content="MsiwZHuFSJ2SGGT-MzDAiPwWZmK2F0PiLWwOsHb78Ck" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
