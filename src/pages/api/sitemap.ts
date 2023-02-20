4c4ee8e6fd90615d2ea4c8ec9da9da19965d1acc
Flo <flodef@free.fr>
2023-02-20 04:19:50 +0100

    Updated sitemap

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -8,19 +8,24 @@ export default function handler(request: NextApiRequest, response: NextApiRespon
     response.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600');
 
     // generate sitemap here
+    const date = new Date().toISOString();
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
         <url>
             <loc>https://pay.fims.fi/new</loc>
-            <lastmod>2023-02-18T21:03:30+00:00</lastmod>
+            <lastmod>${date}</lastmod>
         </url>
         <url>
             <loc>https://pay.fims.fi/processing</loc>
-            <lastmod>2023-02-18T21:03:30+00:00</lastmod>
+            <lastmod>${date}</lastmod>
         </url>
         <url>
             <loc>https://pay.fims.fi/transactions</loc>
-            <lastmod>2023-02-18T21:03:30+00:00</lastmod>
+            <lastmod>${date}</lastmod>
+        </url>
+        <url>
+            <loc>https://pay.fims.fi/merchants</loc>
+            <lastmod>${date}</lastmod>
         </url>
     </urlset>`;
 
