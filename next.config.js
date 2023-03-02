f5396c384bb72d0a694af31c99657dae7858f0be
Flo <flodef@free.fr>
2023-03-02 04:54:29 +0100

    Improve modularize import

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     reactStrictMode: true,
     modularizeImports: {
         '@mui/material': {
-            transform: '@mui/material/dist/{{member}}.d.ts',
+            transform: '@mui/material/{{member}}',
         },
     },
     async redirects() {
