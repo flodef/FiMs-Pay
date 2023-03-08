be48ec504612c39726a8a6bbe10ae54313ebe650
Flo <flodef@free.fr>
2023-03-08 20:29:10 +0100

    Add a link to about page in powered by label

--- src/client/components/sections/PoweredBy.tsx
+++ src/client/components/sections/PoweredBy.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { SOLANA_PAY } from '../../utils/constants';
-import { APP_TITLE } from '../../utils/env';
+import { ABOUT, APP_TITLE } from '../../utils/env';
 import { FiMsPayLogo } from '../images/FiMsPayLogo';
 import { SolanaPayLogo } from '../images/SolanaPayLogo';
 import css from './PoweredBy.module.css';
@@ -9,8 +9,13 @@ import css from './PoweredBy.module.css';
 export const PoweredBy: FC = () => {
     return (
         <div className={css.root}>
-            <FormattedMessage id="poweredBy" />
-            {APP_TITLE === SOLANA_PAY ? <SolanaPayLogo /> : <FiMsPayLogo />}
+            <a className={css.link} href={ABOUT} target="_blank" rel="noreferrer">
+                <div className={css.text}>
+                    <FormattedMessage id="poweredBy" />
+                    &nbsp;
+                </div>
+                {APP_TITLE === SOLANA_PAY ? <SolanaPayLogo /> : <FiMsPayLogo />}
+            </a>
         </div>
     );
 };
