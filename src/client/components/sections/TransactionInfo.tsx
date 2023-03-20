a8bdddb58930e64b9c9bc921d0a6fe2d307cb675
Flo <flodef@free.fr>
2023-03-20 17:44:58 +0100

    Transaction wrong label / amount after scanning bug fix

--- src/client/components/sections/TransactionInfo.tsx
+++ src/client/components/sections/TransactionInfo.tsx
@@ -1,13 +1,28 @@
-import { FC, useMemo } from 'react';
-import { useConfig } from '../../hooks/useConfig';
+import { parseURL } from '@solana/pay';
+import BigNumber from 'bignumber.js';
+import { FC, useEffect, useMemo, useState } from 'react';
 import { PaymentStatus, usePayment } from '../../hooks/usePayment';
 import { Amount } from './Amount';
 import css from './TransactionInfo.module.css';
 
 export const TransactionInfo: FC = () => {
-    const { paymentStatus, isRecipient } = usePayment();
-    const { label } = useConfig();
-    const { amount, isPaidStatus } = usePayment();
+    const { paymentStatus, isRecipient, url } = usePayment();
+    const { isPaidStatus } = usePayment();
+    const [label, setLabel] = useState<string>();
+    const [amount, setAmount] = useState<BigNumber>();
+
+    useEffect(() => {
+        const request = parseURL(url);
+
+        if ('link' in request) {
+            throw new Error('Link request is not supported yet.');
+        } else {
+            const { label, amount } = request;
+            setLabel(label);
+            setAmount(amount);
+        }
+    }, [url]);
+
     const date = useMemo(
         () =>
             new Intl.DateTimeFormat(undefined, {
@@ -22,6 +37,7 @@ export const TransactionInfo: FC = () => {
     );
     const isNewStatus = useMemo(() => paymentStatus === PaymentStatus.New, [paymentStatus]);
 
+    // TODO : add user name
     return (
         <div className={css.root}>
             <div className={isPaidStatus ? css.date : css.dateHidden}>{date}</div>
